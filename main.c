d434ffd713e104a6cd60b55a0c0849339f05d30a
Jesse Palmer <jesselpalmer@gmail.com>
2020-03-19 13:54:17 -0700

    feat: add loop

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void loop();
+
 int main(int agrc, char **argv) {
   printf("The number of arguements is: %d\n", agrc);
+  loop();
   return EXIT_SUCCESS;
+}
+
+void loop() {
+  char cmd[20];
+  printf("> ");
+  scanf("%s", cmd);
+  printf("The command you entered is %s.", cmd);
 }
